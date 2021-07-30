670666ef93146df70101e1eb1c2a52eaf41cc768
slavko <slavko@zagorac.com>
2021-07-30 10:29:41 +0100

    web score overlays, work in progress

--- szcore-gui/src/main/java/com/xenaksys/szcore/gui/view/ScoreController.java
+++ szcore-gui/src/main/java/com/xenaksys/szcore/gui/view/ScoreController.java
@@ -9,7 +9,17 @@ import com.xenaksys.szcore.event.web.audience.WebAudienceInstructionsEvent;
 import com.xenaksys.szcore.gui.SzcoreClient;
 import com.xenaksys.szcore.gui.model.Participant;
 import com.xenaksys.szcore.gui.model.WebscoreInstructions;
-import com.xenaksys.szcore.model.*;
+import com.xenaksys.szcore.model.Bar;
+import com.xenaksys.szcore.model.Beat;
+import com.xenaksys.szcore.model.Clock;
+import com.xenaksys.szcore.model.EventService;
+import com.xenaksys.szcore.model.Id;
+import com.xenaksys.szcore.model.Instrument;
+import com.xenaksys.szcore.model.Page;
+import com.xenaksys.szcore.model.Score;
+import com.xenaksys.szcore.model.ScoreService;
+import com.xenaksys.szcore.model.Tempo;
+import com.xenaksys.szcore.model.TempoModifier;
 import com.xenaksys.szcore.model.id.BarId;
 import com.xenaksys.szcore.model.id.BeatId;
 import com.xenaksys.szcore.model.id.PageId;
@@ -22,7 +32,19 @@ import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
@@ -32,7 +54,13 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.InetAddress;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import static com.xenaksys.szcore.Consts.EMPTY;
 
@@ -395,11 +423,11 @@ public class ScoreController {
             @Override
             public String toString(Double n) {
                 if (n < 10.0) return "tp";
-                if (n < 35.0) return "bb";
-                if (n < 41.0) return "ob";
-                if (n < 46.0) return "msp";
-                if (n < 61.0) return "sp";
-                if (n < 76.0) return "ord";
+                if (n < 32.0) return "bb";
+                if (n < 38.0) return "ob";
+                if (n < 42.0) return "msp";
+                if (n < 70.0) return "sp";
+                if (n < 80.0) return "ord";
                 if (n < 90.0) return "st";
                 return "mst";
             }
